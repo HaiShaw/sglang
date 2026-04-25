a60dfdab9cdd2488366bf27e96297020b483f7bd
Thomas Wang <1am9trash@gmail.com>
2026-04-25 18:03:17 +0800

    Add tilelang backend code path

--- python/sglang/srt/layers/attention/debug_flash_mla_adapter.py
+++ python/sglang/srt/layers/attention/debug_flash_mla_adapter.py
@@ -12,6 +12,9 @@ def flash_mla_with_kvcache_entrypoint(backend: str, **kwargs):
     if is_hip():
         # backend == "torch"
         import os
+        from sglang.srt.layers.attention.nsa.tilelang_kernel import (
+            dpsk_v4_bf16_sparse_attention_fwd,
+        )
 
         backend = os.environ.get("SGLANG_HACK_FLASHMLA_BACKEND", "kernel")
     else:
@@ -31,6 +34,9 @@ def flash_mla_with_kvcache_entrypoint(backend: str, **kwargs):
 
     if backend == "torch":
         return flash_mla_with_kvcache_torch(**kwargs)
+
+    if backend == "tilelang":
+        return dpsk_v4_bf16_sparse_attention_fwd(**kwargs)
 
     if backend == "kernel":
         return flash_mla.flash_mla_with_kvcache(**kwargs)
